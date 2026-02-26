fe7e71847688b0d00d4544f7c2bf88652d827884
randomcode30 <adithwhoyouspam@gmail.com>
2026-02-26 12:39:48 +0530

    Add LED blinking program for Arduino

---
+++ program1.c
@@ -0,0 +1,13 @@
+// LED Blinking Program
+int ledPin = 13; // Built-in LED on most Arduino boards
+void setup()
+{
+pinMode(ledPin, OUTPUT); // Set pin 13 as an output
+}
+void loop()
+{
+digitalWrite(ledPin, HIGH); // Turn LED ON
+delay(1000); // Wait for 1 second
+digitalWrite(ledPin, LOW); // Turn LED OFF
+delay(1000); // Wait for 1 second
+}
